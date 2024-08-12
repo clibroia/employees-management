4079ff6513c7e40e57ad8ba84008cc8b560bccd2
clibroia <carmine87.italy@gmail.com>
2024-08-12 10:28:06 +0200

    Export eslint-config rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigESLint from "eslint-config-eslint";
+import eslintConfigEslintFormatting from "eslint-config-eslint/formatting";
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  ...eslintConfigESLint,
+  eslintConfigEslintFormatting
 ];
