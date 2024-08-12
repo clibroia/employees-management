ef58616653bc9223e341dbed50001226e4a4947e
clibroia <carmine87.italy@gmail.com>
2024-08-12 13:38:32 +0200

    Update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,23 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import {configs, pluginJSConfigs} from "@eslint/js";
 import eslintConfigESLint from "eslint-config-eslint";
 import eslintConfigEslintFormatting from "eslint-config-eslint/formatting";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...eslintConfigESLint,
-  eslintConfigEslintFormatting
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    extends: [
+      pluginJSConfigs.recommended,
+      ...eslintConfigESLint,
+      eslintConfigEslintFormatting
+    ]
+  }
 ];
