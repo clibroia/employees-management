dc71ca6fb33566104880ecf8a19902495384ecdd
clibroia <carmine87.italy@gmail.com>
2024-08-13 21:33:36 +0200

    Add mocha environment to eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import pluginJs from "@eslint/js";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.mocha } } },
   pluginJs.configs.recommended,
 ];
